9eeee1466f358b5d050e208911a2d982f56dbe2e
inna_yan <inna_yan@TeamCity>
2019-12-03 17:54:52 +0300

    TeamCity change in 'Cleanup9a / Cleanup9' project: Using version control synchronization settings from a parent project

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -36,4 +36,64 @@ project {
             preserveArtifactsDependencies = true
         }
     }
+
+    subProject(Cleanup9)
 }
+
+
+object Cleanup9 : Project({
+    name = "Cleanup9"
+
+    buildType(Cleanup9_Cleanup9con)
+
+    cleanup {
+        keep {
+            id = "KEEP_RULE_128"
+            keepAtLeast = allBuilds()
+            dataToKeep = everything()
+            preserveArtifactsDependencies = true
+        }
+    }
+})
+
+object Cleanup9_Cleanup9con : BuildType({
+    name = "cleanup9con"
+
+    vcs {
+        root(DslContext.settingsRoot)
+        root(AbsoluteId("GitJavaEclipse"))
+    }
+
+    cleanup {
+        keep {
+            id = "KEEP_RULE_118"
+            keepAtLeast = builds(5)
+            dataToKeep = everything()
+            preserveArtifactsDependencies = true
+        }
+        keep {
+            id = "KEEP_RULE_130"
+            keepAtLeast = allBuilds()
+            applyToBuilds {
+                inBranches("""
+                    +:*overrie\den
+                    +:*overrie\den
+                    +:*overrie\den
+                    +:*overrie\den
+                    +:*overrie\den
+                """.trimIndent())
+                inPersonalBuilds = personal()
+                withStatus = successful()
+            }
+            dataToKeep = historyAndStatistics {
+                preserveArtifacts = byPattern("""
+                    +:cleanup9aconfigoverriden/cleanup9aconfigooverridedn/**.*
+                    +:cleanup9aconfigoverriden/cleanup9aconfigoverriden/**.*
+                """.trimIndent())
+            }
+            applyPerEachBranch()
+            preserveArtifactsDependencies = true
+        }
+        option("disableCleanupPolicies", true)
+    }
+})
